35a59325bed183742a3132d5198f0d5cc97f8973
Barnabas27 <bnyambura032@gmail.com>
2021-10-25 11:29:45 +0300

    add corrections

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,32 +1,44 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * selection_sort - sorts an array of integers in ascending order using
- * selection sort
- * @array: the array to be sorted
- * @size: size of the array to be sorted
- */
-
+* selection_sort - sorts an array of integers in ascending order
+* using the Selection sort algorithm
+* @array: pointer to the array
+* @size: size of the array
+*/
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
 
-	if (size == 0 || (!array))
-		return;
+size_t i, j, jMin;
 
-	for (i = 0; i < size; i++)
+for (i = 0; i <= size; i++)
+{
+	jMin = i; /*consider the first element as min*/
+	for (j = i + 1; j < size; j++)
 	{
-		j = i;
-		for (; j < size; j++)
-		{
-			if (array[j] < array[i] && array[j] != array[0])
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
-		}
+		if (array[j] < array[jMin])
+			jMin = j;
 	}
+	if (jMin != i)
+	{
+		swap_int(array, i, jMin);
+		print_array(array, size);
+	}
+}
+
+}
+
+/**
+* swap_int - swap variable values
+* @array: array to use
+* @a: index 1
+* @b: index 2
+*/
+void swap_int(int *array, size_t a, size_t b)
+{
+int tmp;
+tmp = array[a];
+array[a] = array[b];
+array[b] = tmp;
 }
--- sort.h
+++ sort.h
@@ -26,6 +26,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap(listint_t *node1, listint_t *node2, listint_t **head);
+void swap_int(int *array, size_t a, size_t b);
+void swap_int1(int *array, int a, int b);
 void insertion_sort_list(listint_t **list);
 int partition(int *arr, int start_idx, int last_idx, int size);
 void quick(int *arr, int start_idx, int last_idx, int size);
